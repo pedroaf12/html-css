6a8ee0291bce81ee28da0dde9dd74c3389ef34c8
pedroaf12 <51967841+pedroaf12@users.noreply.github.com>
2024-08-30 17:02:54 -0300

    exercício 3 de formulário

--- exercicios/ex025/cadastro.js
+++ exercicios/ex025/cadastro.js
@@ -1,9 +1,16 @@
 document.getElementById('formulario').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const user = document.getElementById('iusuario').value;
-    const key = document.getElementById('isenha').value;
+    // const user = document.getElementById('iusuario').value;
+    // const key = document.getElementById('isenha').value;
+
 
     // output.textContent = `Usuário ${user} e Senha ${key}`
-    console.log(user,key);
+    // console.log(user,key);
+
+    const media = document.getElementById('imedia').value;
+    const nome = document.getElementById('inome').value;
+    output.textContent = `Usuário: ${nome} e Media ${media}`
+    
 });
+// document.body.innerText = ('name' in user)
